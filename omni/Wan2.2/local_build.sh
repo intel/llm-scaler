e21501ed7db569b37d97b02ef86699dc004a02a7
Arcs-ur <arcs-ur@sjtu.edu.cn>
2025-09-09 11:22:06 +0800

    fix

--- omni/Wan2.2/local_build.sh
+++
@@ -1,6 +0,0 @@
-set -x
-
-export HTTP_PROXY=http://proxy.iil.intel.com:911
-export HTTPS_PROXY=http://proxy.iil.intel.com:911
-
-docker build -f ./docker/Dockerfile . -t llm-scaler-visualai:latest-wan2.2 --build-arg https_proxy=$HTTPS_PROXY --build-arg http_proxy=$HTTP_PROXY
