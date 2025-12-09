ff4254c86a3a6fd9777d40c5f35541e79a10f3f6
xiangyuT <xiangyu.tian@intel.com>
2025-12-09 13:56:20 +0800

    fix

--- omni/entrypoints/start_comfyui.sh
+++ omni/entrypoints/start_comfyui.sh
@@ -2,4 +2,4 @@ export http_proxy=<your_http_proxy>
 export https_proxy=<your_https_proxy>
 export no_proxy=localhost,127.0.0.1
 
-python main.py --listen 0.0.0.0 --port 8188
+python /llm/ComfyUI/main.py --listen 0.0.0.0 --port 8188
