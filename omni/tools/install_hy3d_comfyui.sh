7de8eeb19ca2138c261d72eefae5ba6d80a10571
Xiangyu Tian <109123695+xiangyuT@users.noreply.github.com>
2025-12-18 14:25:46 +0800

    Update install_hy3d_comfyui.sh

--- omni/tools/install_hy3d_comfyui.sh
+++ omni/tools/install_hy3d_comfyui.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/visualbruno/ComfyUI-Hunyuan3d-2-1.git && \
 cd ComfyUI-Hunyuan3d-2-1 && \
 git checkout 9d7ef32509101495a7840b3ae8e718c8d1183305 && \
-git apply /tmp/comfyui_hunyuan3d_for_xpu.patch && \
+wget https://github.com/intel/llm-scaler/raw/refs/heads/main/omni/patches/comfyui_hunyuan3d_for_xpu.patch && \
+git apply ./comfyui_hunyuan3d_for_xpu.patch && \
 pip install bigdl-core==2.4.0b1 rembg realesrgan && \
 pip install -r requirements.txt && \
 cd hy3dpaint/custom_rasterizer && \
