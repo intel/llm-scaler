747f765fec5533cdbbccd978f0d17a9824e9d0f8
xiangyuT <xiangyu.tian@intel.com>
2026-03-19 04:56:50 +0000

    update dockerfile

--- omni/build.sh
+++ omni/build.sh
@@ -3,4 +3,6 @@ set -x
 export HTTP_PROXY=<your_http_proxy>
 export HTTPS_PROXY=<your_https_proxy>
 
-docker build -f ./docker/Dockerfile . -t intel/llm-scaler-omni:0.1.0-b5 --build-arg https_proxy=$HTTPS_PROXY --build-arg http_proxy=$HTTP_PROXY
+TAG="0.1.0-b7-dev"
+
+DOCKER_BUILDKIT=1 docker build -f ./docker/Dockerfile . -t intel/llm-scaler-omni:$TAG --build-arg https_proxy=$HTTPS_PROXY --build-arg http_proxy=$HTTP_PROXY
